f07c025c20c4e7414da91af026007f4d41a1c7a0
AmbroseQiu <ambroseqiu1027@gmail.com>
2023-05-16 21:45:25 +0800

    add docker-compose, retry db connection

--- repository/db.go
+++ repository/db.go
@@ -1,10 +1,12 @@
 package repository
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"os"
+	"time"
 
-	"github.com/rs/zerolog/log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -17,13 +19,45 @@ func NewGormDB() (*gorm.DB, error) {
 	password := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", host, port, user, password, dbName)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger:         logger.Default.LogMode(logger.Info),
-		TranslateError: true,
-	})
-	if err != nil {
-		log.Fatal().Err(err).Msg("can't not access db with gorm")
+
+	ctx := context.Background()
+	timeout := 5 * time.Second
+
+	// 使用 context.WithTimeout 創建一個帶有超時的子 context
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	done := make(chan error, 1)
+
+	var gormDB *gorm.DB
+	var err error
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				done <- fmt.Errorf("db connection time out")
+				break
+			default:
+				gormDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+					Logger:         logger.Default.LogMode(logger.Info),
+					TranslateError: true,
+				})
+				if err == nil {
+					done <- nil
+					break
+				}
+				time.Sleep(500 * time.Millisecond)
+			}
+		}
+	}()
+
+	select {
+	case err := <-done:
+		if err != nil {
+			return nil, errors.New("can't not access db with gorm")
+		}
 	}
 
-	return db, nil
+	return gormDB, nil
 }
